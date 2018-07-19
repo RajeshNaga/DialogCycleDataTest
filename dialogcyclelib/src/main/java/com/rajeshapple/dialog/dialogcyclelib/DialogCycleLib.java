0d6aa2d922e26ba213b595a3f8afaa47337f5fdc
Rajesh <rajesh@speedkyat.com>
2018-07-19 23:49:58 +0630

    f commit

---
+++ dialogcyclelib/src/main/java/com/rajeshapple/dialog/dialogcyclelib/DialogCycleLib.java
@@ -0,0 +1,14 @@
+package com.rajeshapple.dialog.dialogcyclelib;
+
+import android.util.Log;
+
+/**
+ * Created by super on 7/19/18.
+ */
+
+public class DialogCycleLib {
+    public void DataValue(String msg)
+    {
+        Log.d(DialogCycleLib.this.getClass().getSimpleName(),msg);
+    }
+}
